3d2a218df7af67186706a093a46d0ab50d6e8fb0
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-19 17:36:37 +0200

    Adds abstract base entity to repo

---
+++ src/main/java/com/codinginfinity/benchmark/managenent/domain/AbstractBaseEntity.java
@@ -0,0 +1,14 @@
+package com.codinginfinity.benchmark.managenent.domain;
+
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * Created by andrew on 2016/06/15.
+ */
+@EqualsAndHashCode
+public abstract class AbstractBaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -4535278797598172274L;
+}
