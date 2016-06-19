fd350ace631703e6156dc2d67698593b6d575c47
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-19 21:15:58 +0200

    Adds unit test for RequestPasswordReset use case in UserManagement module

---
+++ src/test/java/com/codinginfinity/benchmark/management/service/userManagement/RequestPasswordResetTest.java
@@ -0,0 +1,116 @@
+package com.codinginfinity.benchmark.management.service.userManagement;
+
+import com.codinginfinity.benchmark.managenent.ManagementApp;
+import com.codinginfinity.benchmark.managenent.domain.User;
+import com.codinginfinity.benchmark.managenent.repository.UserRepository;
+import com.codinginfinity.benchmark.managenent.security.UserNotActivatedException;
+import com.codinginfinity.benchmark.managenent.service.userManagement.exceptions.NotAuthorizedException;
+import com.codinginfinity.benchmark.managenent.service.userManagement.UserManagement;
+import com.codinginfinity.benchmark.managenent.service.userManagement.request.RequestPasswordResetRequest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by andrew on 2016/06/19.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ManagementApp.class)
+public class RequestPasswordResetTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    @Inject
+    private UserManagement userManagement;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void userNotRegisteredTest() throws NotAuthorizedException, UserNotActivatedException {
+        Mockito.when(userRepository.findOneByEmail("johndoe@example.com")).thenReturn(Optional.empty());
+        thrown.expect(NotAuthorizedException.class);
+        thrown.expectMessage("Invalid email address");
+
+        userManagement.requestPasswordReset(new RequestPasswordResetRequest("johndoe@example.com"));
+    }
+
+    @Test
+    public void userNotActivatedTest() throws NotAuthorizedException, UserNotActivatedException {
+        User user = new User();
+        user.setActivated(false);
+
+        Mockito.when(userRepository.findOneByEmail("johndoe@example.com")).thenReturn(Optional.of(user));
+        thrown.expect(UserNotActivatedException.class);
+        thrown.expectMessage("User is not activated");
+
+        userManagement.requestPasswordReset(new RequestPasswordResetRequest("johndoe@example.com"));
+    }
+
+    @Test
+    public void userHasOutstandingResetRequests() throws NotAuthorizedException, UserNotActivatedException {
+        User user = new User();
+        user.setActivated(true);
+        user.setResetDate(ZonedDateTime.now());
+        user.setResetKey("1234567890");
+
+        Mockito.when(userRepository.findOneByEmail("johndoe@example.com")).thenReturn(Optional.of(user));
+        thrown.expect(NotAuthorizedException.class);
+        thrown.expectMessage("User has outstanding reset requests");
+
+        userManagement.requestPasswordReset(new RequestPasswordResetRequest("johndoe@example.com"));
+    }
+
+    @Test
+    public void resetUserPassword() throws NotAuthorizedException, UserNotActivatedException {
+        User user = new User();
+        user.setUsername("johndoe");
+        user.setPassword("p@$$w0rd");
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setEmail("johndoe@example.com");
+        user.setActivated(true);
+        user.setResetDate(null);
+        user.setResetKey(null);
+
+        Mockito.when(userRepository.findOneByEmail("johndoe@example.com")).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        Optional<User> resetUser = userManagement.requestPasswordReset(new RequestPasswordResetRequest("johndoe@example.com")).getUser();
+
+        /* Assert that only required properties on the object has changed, no more and no less */
+        assertTrue(resetUser.isPresent());
+        assertEquals(user.getUsername(), resetUser.get().getUsername());
+        assertEquals(user.getPassword(), resetUser.get().getPassword());
+        assertEquals(user.getFirstName(), resetUser.get().getFirstName());
+        assertEquals(user.getLastName(), resetUser.get().getLastName());
+        assertEquals(user.getEmail(), resetUser.get().getEmail());
+        assertEquals(user.isActivated(), resetUser.get().isActivated());
+        /* The properties below should be now different on the new entity */
+        assertNotNull(resetUser.get().getResetKey());
+        assertNotNull(resetUser.get().getResetDate());
+        assertTrue(ZonedDateTime.now().minusHours(1).isBefore(resetUser.get().getResetDate()));
+    }
+}
