1cc96dcce8d29b76353c5f60964ddae195c37153
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-20 21:42:54 +0200

    Setup ThymeleafConfiguration bean for notification module

---
+++ src/main/java/com/codinginfinity/benchmark/managenent/config/ThymeleafConfiguration.java
@@ -0,0 +1,28 @@
+package com.codinginfinity.benchmark.managenent.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.CharEncoding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+/**
+ * Created by andrew on 2016/06/20.
+ */
+@Configuration
+@Slf4j
+public class ThymeleafConfiguration {
+
+    @Bean
+    @Description("Thymeleaf template resolver serving HTML 5 emails")
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+        emailTemplateResolver.setPrefix("mails/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode("HTML5");
+        emailTemplateResolver.setCharacterEncoding(CharEncoding.UTF_8);
+        emailTemplateResolver.setOrder(1);
+        return emailTemplateResolver;
+    }
+}
