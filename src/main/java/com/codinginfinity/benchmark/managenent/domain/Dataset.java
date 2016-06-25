2e72ef035abed45f19962b3f703217b2e3cab52f
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-25 23:06:23 +0200

    added DatasetRepository

--- src/main/java/com/codinginfinity/benchmark/managenent/domain/Dataset.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/domain/Dataset.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
@@ -25,6 +22,10 @@ import java.util.List;
 public class Dataset implements Serializable {
 
     private static final long serialVersionUID = -5132732623123018351L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotNull
     private String name;
