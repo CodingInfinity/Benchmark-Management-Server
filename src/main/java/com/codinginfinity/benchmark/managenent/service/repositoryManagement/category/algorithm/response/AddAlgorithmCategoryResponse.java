92fdd69fc0abee2aaca31b3ece90036c0c277e4c
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-26 20:53:20 +0200

    Updates Algorithm Category response objects

--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/algorithm/response/AddAlgorithmCategoryResponse.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/algorithm/response/AddAlgorithmCategoryResponse.java
@@ -1,14 +1,14 @@
 package com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.algorithm.response;
 
 import com.codinginfinity.benchmark.managenent.service.Response;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by andrew on 2016/06/25.
  */
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class AddAlgorithmCategoryResponse extends Response {
 
     private static final long serialVersionUID = 1550126234165793236L;
