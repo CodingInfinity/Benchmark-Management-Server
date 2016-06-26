d29f0d3544fcdbd294c0ee1f9e67e0ec484eb1ad
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-26 23:00:15 +0200

    Implements unit test for DeleteAlgorithmCategory

---
+++ src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/category/algorithm/DeleteAlgorithmCategoryTest.java
@@ -0,0 +1,27 @@
+package com.codinginfinity.benchmark.management.service.repositoryManagement.category.algorithm;
+
+import com.codinginfinity.benchmark.management.service.repositoryManagement.category.DeleteCategoryTest;
+import com.codinginfinity.benchmark.managenent.domain.AlgorithmCategory;
+import com.codinginfinity.benchmark.managenent.repository.AlgorithmCategoryRepository;
+import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.algorithm.AlgorithmCategoryManagement;
+
+/**
+ * Created by andrew on 2016/06/25.
+ */
+public class DeleteAlgorithmCategoryTest extends DeleteCategoryTest<AlgorithmCategory, AlgorithmCategoryRepository, AlgorithmCategoryManagement> {
+
+    @Override
+    protected Long getExpectedId() {
+        return 1L;
+    }
+
+    @Override
+    protected String getExpectedName() {
+        return "Test";
+    }
+
+    @Override
+    protected AlgorithmCategory getCategory() {
+        return new AlgorithmCategory(getExpectedId(), getExpectedName());
+    }
+}
