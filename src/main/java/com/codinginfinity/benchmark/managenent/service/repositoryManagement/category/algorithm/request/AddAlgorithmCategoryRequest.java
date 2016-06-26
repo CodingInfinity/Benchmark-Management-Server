39bf1a566e264084c9c1353029c01819aaba67ce
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-26 20:53:07 +0200

    Updates Algorithm Category request objects

--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/algorithm/request/AddAlgorithmCategoryRequest.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/algorithm/request/AddAlgorithmCategoryRequest.java
@@ -1,11 +1,19 @@
 package com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.algorithm.request;
 
 import com.codinginfinity.benchmark.managenent.service.Request;
+import lombok.*;
 
 /**
  * Created by andrew on 2016/06/25.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class AddAlgorithmCategoryRequest extends Request {
 
     private static final long serialVersionUID = -4672903565291369727L;
+
+    private String name;
 }
