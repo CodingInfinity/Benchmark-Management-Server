b2eb92ad765b8fb573191ac8f881b9205c3dc60d
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-26 21:42:42 +0200

    Request and response objects for GetAlgorithmById

--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/algorithm/response/GetAlgorithmByIdResponse.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/algorithm/response/GetAlgorithmByIdResponse.java
@@ -1,11 +1,18 @@
 package com.codinginfinity.benchmark.managenent.service.repositoryManagement.algorithm.response;
 
+import com.codinginfinity.benchmark.managenent.domain.Algorithm;
 import com.codinginfinity.benchmark.managenent.service.Response;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * Created by andrew on 2016/06/25.
  */
+@Getter
+@AllArgsConstructor
 public class GetAlgorithmByIdResponse extends Response {
 
     private static final long serialVersionUID = -6185910613063888087L;
+
+    Algorithm algorithm;
 }
