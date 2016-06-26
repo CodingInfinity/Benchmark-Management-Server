cf1b3c0186ccfa5ab7e0fb73f2f9c9bee232dac7
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-26 12:03:23 +0200

    Adds necessary annotations to Dataset Category response objects

--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/dataet/response/GetDatasetCategoryResponse.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/dataet/response/GetDatasetCategoryResponse.java
@@ -1,7 +1,15 @@
 package com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.dataet.response;
 
+import com.codinginfinity.benchmark.managenent.service.Response;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by andrew on 2016/06/25.
  */
-public class GetDatasetCategoryResponse {
+@Getter
+@NoArgsConstructor
+public class GetDatasetCategoryResponse extends Response {
+
+    private static final long serialVersionUID = 2822614308213800378L;
 }
