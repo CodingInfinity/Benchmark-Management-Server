17c0338f5207821cda2bd9fba9a95832cbc5d797
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-26 23:01:04 +0200

    Implements unit test for UpdateAlgorithmCategory

---
+++ src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/category/algorithm/UpdateAlgorithmCategoryTest.java
@@ -0,0 +1,27 @@
+package com.codinginfinity.benchmark.management.service.repositoryManagement.category.algorithm;
+
+import com.codinginfinity.benchmark.management.service.repositoryManagement.category.UpdateCategoryTest;
+import com.codinginfinity.benchmark.managenent.domain.AlgorithmCategory;
+import com.codinginfinity.benchmark.managenent.repository.AlgorithmCategoryRepository;
+import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.algorithm.AlgorithmCategoryManagement;
+
+/**
+ * Created by andrew on 2016/06/25.
+ */
+public class UpdateAlgorithmCategoryTest extends UpdateCategoryTest<AlgorithmCategory, AlgorithmCategoryRepository, AlgorithmCategoryManagement> {
+
+    @Override
+    protected Long getExpectedId() {
+        return 1L;
+    }
+
+    @Override
+    protected String getExpectedName() {
+        return "Test";
+    }
+
+    @Override
+    protected AlgorithmCategory getCategory() {
+        return new AlgorithmCategory(getExpectedId(), getExpectedName());
+    }
+}
