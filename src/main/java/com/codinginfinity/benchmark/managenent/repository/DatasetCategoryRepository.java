5422592c9d481ad598f937ba3af81407db8ee309
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-26 12:07:08 +0200

    Adds findOneByName query to add DatasetCategoryRepository

--- src/main/java/com/codinginfinity/benchmark/managenent/repository/DatasetCategoryRepository.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/repository/DatasetCategoryRepository.java
@@ -4,9 +4,14 @@ import com.codinginfinity.benchmark.managenent.domain.DatasetCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Created by andrew on 2016/06/25.
  */
 @Repository
 public interface DatasetCategoryRepository extends JpaRepository<DatasetCategory, Long> {
+
+    Optional<DatasetCategory> findOneByName(String name);
 }
+
