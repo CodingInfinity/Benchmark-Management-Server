50a177f159c92593b4eee244c4b2b7e9f7fa153e
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-26 12:06:48 +0200

    Adds findOneByName query to add AlgorithmCategoryRepository

--- src/main/java/com/codinginfinity/benchmark/managenent/repository/AlgorithmCategoryRepository.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/repository/AlgorithmCategoryRepository.java
@@ -4,9 +4,13 @@ import com.codinginfinity.benchmark.managenent.domain.AlgorithmCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Created by andrew on 2016/06/25.
  */
 @Repository
 public interface AlgorithmCategoryRepository extends JpaRepository<AlgorithmCategory, Long> {
+
+    Optional<AlgorithmCategory> findOneByName(String name);
 }
