42eb47d82b252813af2505bedde155225372073c
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-26 18:42:36 +0200

    AlgortihmRepository Implemented

--- src/main/java/com/codinginfinity/benchmark/managenent/repository/AlgorithmRepository.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/repository/AlgorithmRepository.java
@@ -1,12 +1,23 @@
 package com.codinginfinity.benchmark.managenent.repository;
 
 import com.codinginfinity.benchmark.managenent.domain.Algorithm;
+import com.codinginfinity.benchmark.managenent.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by reinhardt on 2016/06/25.
  */
 @Repository
 public interface AlgorithmRepository extends JpaRepository<Algorithm, Long> {
+    Optional<Algorithm> findOneById(Long id);
+
+    Optional<Algorithm> findOneByName(String name);
+
+    List<Optional<Algorithm>> findByUser(String user);
+
+    List<Optional<Algorithm>> findByCategory(Category category);
 }
