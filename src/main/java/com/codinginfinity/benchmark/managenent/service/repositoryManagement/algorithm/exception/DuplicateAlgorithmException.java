1779aa26b33b4c4660baaf75763022ed9dfc0ac3
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-26 19:46:46 +0200

    AddAlgorithmReques added

---
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/algorithm/exception/DuplicateAlgorithmException.java
@@ -0,0 +1,7 @@
+package com.codinginfinity.benchmark.managenent.service.repositoryManagement.algorithm.exception;
+
+/**
+ * Created by reinhardt on 2016/06/26.
+ */
+public class DuplicateAlgorithmException {
+}
--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/algorithm/request/AddAlgorithmRequest.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/algorithm/request/AddAlgorithmRequest.java
@@ -1,12 +1,25 @@
 package com.codinginfinity.benchmark.managenent.service.repositoryManagement.algorithm.request;
 
+import com.codinginfinity.benchmark.managenent.domain.AlgorithmCategory;
+import com.codinginfinity.benchmark.managenent.domain.User;
 import com.codinginfinity.benchmark.managenent.service.Request;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
 
 /**
  * Created by andrew on 2016/06/25.
  */
+@Getter
+@AllArgsConstructor
 public class AddAlgorithmRequest extends Request {
 
     private static final long serialVersionUID = 7769164294013216788L;
 
+    String name;
+
+    User user;
+
+    List<AlgorithmCategory> categories;
 }
