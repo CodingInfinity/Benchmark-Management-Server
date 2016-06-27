8152fb32e37a6ab8b443bdaf9713d354b2e4508e
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-27 15:56:05 +0200

    AddRepoEntityTest

--- src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/AbstractRepositoryManagementTest.java
+++ src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/AbstractRepositoryManagementTest.java
@@ -1,7 +1,25 @@
 package com.codinginfinity.benchmark.management.service.repositoryManagement;
 
+import com.codinginfinity.benchmark.management.AbstractTest;
+import com.codinginfinity.benchmark.managenent.domain.Category;
+import com.codinginfinity.benchmark.managenent.domain.RepoEntity;
+import com.codinginfinity.benchmark.managenent.domain.User;
+
+import java.util.List;
+
 /**
  * Created by reinhardt on 2016/06/27.
  */
-public class AbstractRepositoryManagementTest {
+public abstract class AbstractRepositoryManagementTest <T extends RepoEntity> extends AbstractTest {
+    protected abstract Long getExpectedId();
+
+    protected abstract String getExpectedName();
+
+    protected abstract String getExpectedDescription();
+
+    protected abstract User getExpectedUser();
+
+    protected abstract List<Category> getExpectedCategories();
+
+    protected abstract T getRepoEntity();
 }
--- src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/AddRepoEntityTest.java
+++ src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/AddRepoEntityTest.java
@@ -1,7 +1,59 @@
 package com.codinginfinity.benchmark.management.service.repositoryManagement;
 
+import com.codinginfinity.benchmark.managenent.domain.RepoEntity;
+import com.codinginfinity.benchmark.managenent.repository.RepoEntityRepository;
+import com.codinginfinity.benchmark.managenent.service.repositoryManagement.RepositoryEntityManagement;
+import com.codinginfinity.benchmark.managenent.service.repositoryManagement.request.AddRepoEntityRequest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+
 /**
  * Created by reinhardt on 2016/06/27.
  */
-public class AddRepoEntityTest {
+public abstract class AddRepoEntityTest<T extends RepoEntity,
+        R extends RepoEntityRepository<T>,
+        M extends RepositoryEntityManagement<T>> extends AbstractRepositoryManagementTest<T> {
+
+    @InjectMocks
+    @Inject
+    M repositoryEntityManagement;
+
+    @Mock
+    R repoEntityRepository;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void addRepoEntityTest(){
+        Mockito.when(repoEntityRepository.save((T)any())).thenAnswer(invocation -> {
+            T entity = (T)invocation.getArguments()[0];
+            entity.setId(getExpectedId());
+            return entity;
+        });
+
+        T entity = repositoryEntityManagement.addRepoEntity(new AddRepoEntityRequest<T>(getExpectedName(),
+                getExpectedUser(), getExpectedCategories(), getExpectedDescription())).getRepoEntity();
+        assertEquals(entity.getId(), new Long(12345));
+       // assertEquals(entity.getCategories().size(), 2);
+        assertEquals(entity.getDescription(), getExpectedDescription());
+        assertEquals(entity.getName(), getExpectedName());
+        assertEquals(entity.getUser().getUsername(), getExpectedUser().getUsername());
+    }
 }
