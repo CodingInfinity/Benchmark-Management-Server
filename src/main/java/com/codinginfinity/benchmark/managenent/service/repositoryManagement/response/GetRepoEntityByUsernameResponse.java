d4eda7cad7eddb2f8f4e7119d17f4a93fa40c922
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-27 15:14:27 +0200

    All Response objects for RepositoryEntityManagement interface

---
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/response/GetRepoEntityByUsernameResponse.java
@@ -0,0 +1,14 @@
+package com.codinginfinity.benchmark.managenent.service.repositoryManagement.response;
+
+import com.codinginfinity.benchmark.managenent.domain.RepoEntity;
+import com.codinginfinity.benchmark.managenent.service.Response;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created by reinhardt on 2016/06/27.
+ */
+@Getter
+@AllArgsConstructor
+public class GetRepoEntityByUsernameResponse <T extends RepoEntity> extends Response {
+}
