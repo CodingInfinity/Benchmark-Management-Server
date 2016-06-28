b7cedfcca129572782f4806d8206854924b7deac
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-28 17:21:37 +0200

    Adds unit test for DeleteAlgorithmCategory use case

--- src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/category/algorithm/DeleteAlgorithmCategoryTest.java
+++ src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/category/algorithm/DeleteAlgorithmCategoryTest.java
@@ -3,13 +3,22 @@ package com.codinginfinity.benchmark.management.service.repositoryManagement.cat
 import com.codinginfinity.benchmark.management.service.repositoryManagement.category.DeleteCategoryTest;
 import com.codinginfinity.benchmark.managenent.domain.AlgorithmCategory;
 import com.codinginfinity.benchmark.managenent.repository.AlgorithmCategoryRepository;
-import com.codinginfinity.benchmark.managenent.repository.CategoryRepository;
 import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.algorithm.AlgorithmCategoryManagement;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by andrew on 2016/06/25.
  */
-public class DeleteAlgorithmCategoryTest extends DeleteCategoryTest<AlgorithmCategory, AlgorithmCategoryRepository, AlgorithmCategoryManagement> {
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(AlgorithmCategorySpringTest.class)
+public class DeleteAlgorithmCategoryTest extends DeleteCategoryTest<AlgorithmCategory,
+        AlgorithmCategoryRepository,
+        AlgorithmCategoryManagement> {
 
     @Override
     protected Long getExpectedId() {
@@ -24,5 +33,15 @@ public class DeleteAlgorithmCategoryTest extends DeleteCategoryTest<AlgorithmCat
     @Override
     protected AlgorithmCategory getCategory() {
         return new AlgorithmCategory(getExpectedId(), getExpectedName());
+    }
+
+    @Override
+    protected String getDuplicateCategoryExceptionMessage() {
+        return "Duplicate algorithm category";
+    }
+
+    @Override
+    protected String getNonExistentCategoryExceptionMessage() {
+        return "Algorithm category doesn't exist";
     }
 }
