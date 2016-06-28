a7ddea44e0165863732dd8938be157cb86ae787b
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-28 17:38:58 +0200

    Defines service contract for CategoryManagement with all associated exceptions, request and response objects

--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/response/GetCategoryByIdResponse.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/response/GetCategoryByIdResponse.java
@@ -1,20 +1,20 @@
 package com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.response;
 
 import com.codinginfinity.benchmark.managenent.domain.Category;
-import com.codinginfinity.benchmark.managenent.service.Request;
+import com.codinginfinity.benchmark.managenent.service.Response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * Created by andrew on 2016/06/26.
+ * Created by andrew on 2016/06/28.
  */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UpdateCategoryResponse<T extends Category> extends Request {
+public class GetCategoryByIdResponse<T extends Category> extends Response {
 
-    private static final long serialVersionUID = 774295711368311221L;
+    private static final long serialVersionUID = -9165616883587596273L;
 
     private T category;
 }
