3c9fb2ecb0debb9ce78da59356033f510c90a6a7
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-28 17:45:12 +0200

    Created test objects for GetUnusedRepoEntityByUserNameTest and GetUnusedRepoEntityTest, no implementation yet

---
+++ src/test/java/com/codinginfinity/benchmark/management/service/repositoryManagement/GetUnusedRepoEntityByUserNameTest.java
@@ -0,0 +1,7 @@
+package com.codinginfinity.benchmark.management.service.repositoryManagement;
+
+/**
+ * Created by reinhardt on 2016/06/28.
+ */
+public class GetUnusedRepoEntityByUserNameTest {
+}
