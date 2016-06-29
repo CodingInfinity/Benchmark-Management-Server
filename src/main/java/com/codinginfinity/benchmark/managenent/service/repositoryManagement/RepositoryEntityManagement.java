54070e92f4c950366d043f714e5aaba8f2a44b74
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-06-29 13:07:49 +0200

    Refactored RepositoryEntityManagement interface to throw correct exceptions

--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/RepositoryEntityManagement.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/RepositoryEntityManagement.java
@@ -2,6 +2,7 @@ package com.codinginfinity.benchmark.managenent.service.repositoryManagement;
 
 import com.codinginfinity.benchmark.managenent.domain.Category;
 import com.codinginfinity.benchmark.managenent.domain.RepoEntity;
+import com.codinginfinity.benchmark.managenent.service.repositoryManagement.exception.NonExistentRepoEntityException;
 import com.codinginfinity.benchmark.managenent.service.repositoryManagement.request.*;
 import com.codinginfinity.benchmark.managenent.service.repositoryManagement.response.*;
 
@@ -10,10 +11,10 @@ import com.codinginfinity.benchmark.managenent.service.repositoryManagement.resp
  */
 public interface RepositoryEntityManagement<C extends Category, T extends RepoEntity<C>> {
     AddRepoEntityResponse<T> addRepoEntity(AddRepoEntityRequest<C, T> request);
-    DeleteRepoEntityResponse<T> deleteRepoEntity(DeleteRepoEntityRequest<T> request);
-    UpdateRepoEntityMetadataResponse<T> updateRepoEntityMetaData(UpdataRepoEntityMetadataRequest<C,T> request);
-    GetRepoEntityByIdResponse<T> getRepoEntityById(GetRepoEntityByIdRequest<T> request);
-    GetRepoEntityByUsernameResponse<T> getRepoEntityByUsername(GetRepoEntityByUsernameRequest<T> request);
+    DeleteRepoEntityResponse<T> deleteRepoEntity(DeleteRepoEntityRequest<T> request) throws NonExistentRepoEntityException;
+    UpdateRepoEntityMetadataResponse<T> updateRepoEntityMetaData(UpdataRepoEntityMetadataRequest<C,T> request) throws NonExistentRepoEntityException;
+    GetRepoEntityByIdResponse<T> getRepoEntityById(GetRepoEntityByIdRequest<T> request) throws NonExistentRepoEntityException;
+    GetRepoEntityByUsernameResponse<T> getRepoEntityByUsername(GetRepoEntityByUsernameRequest<T> request)throws NonExistentRepoEntityException;
     GetRepoEntityByCategoryResponse<T> getRepoEntityByCategory(GetRepoEntityByCategoryRequest<C, T> request);
     GetUnusedRepoEntitysResponse<T> getUnusedRepoEntitys(GetUnusedRepoEntitysRequest<T> request);
     GetUnusedRepoEntityByUsernameResponse<T> getUnusedRepoEntityByUsername(GetUnusedRepoEntityByUsernameRequest<T> request);
