7848514d297cf6c12e4c663a3f3a843a74ab2566
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-29 17:14:34 +0200

    Removes annotations to create generic class

--- src/main/java/com/codinginfinity/benchmark/managenent/web/rest/repositoryManagement/CategoryResource.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/web/rest/repositoryManagement/CategoryResource.java
@@ -1,30 +1,18 @@
 package com.codinginfinity.benchmark.managenent.web.rest.repositoryManagement;
 
-import com.codinginfinity.benchmark.managenent.domain.AlgorithmCategory;
 import com.codinginfinity.benchmark.managenent.domain.Category;
 import com.codinginfinity.benchmark.managenent.repository.CategoryRepository;
 import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.CategoryManagement;
-import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.algorithm.AlgorithmCategoryManagement;
 import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.exception.DuplicateCategoryException;
 import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.exception.NonExistentCategoryException;
 import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.request.*;
-import com.codinginfinity.benchmark.managenent.service.repositoryManagement.category.response.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.inject.Inject;
-import java.util.Optional;
 
 /**
  * Created by andrew on 2016/06/29.
  */
-@RestController
-@RequestMapping("/api/repo/category")
 @Slf4j
 public abstract class CategoryResource<T extends Category, R extends CategoryRepository<T>, S extends CategoryManagement<T>> {
 
