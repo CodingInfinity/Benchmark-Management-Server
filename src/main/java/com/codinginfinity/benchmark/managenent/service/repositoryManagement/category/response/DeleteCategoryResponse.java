637a5b104f5f76e7eff59bc5c75987c13ac863ef
Andrew Broekman <andrewbroekman@gmail.com>
2016-06-29 17:39:22 +0200

    Adds private identifier to DeleteCategoryResponse object

--- src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/response/DeleteCategoryResponse.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/service/repositoryManagement/category/response/DeleteCategoryResponse.java
@@ -10,10 +10,12 @@ import lombok.NoArgsConstructor;
  * Created by andrew on 2016/06/26.
  */
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class DeleteCategoryResponse<T extends Category> extends Request {
 
     private static final long serialVersionUID = -4236881914680237709L;
 
-    T category;
+    private T category;
+
 }
