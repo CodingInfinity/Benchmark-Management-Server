8cf57d3cf4bb77f54ab8b5f4eda33585120eb155
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-01 22:15:40 +0200

    Corrects namespace on CORS filter

--- src/main/java/com/codinginfinity/benchmark/managenent/config/WebConfigurer.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/config/WebConfigurer.java
@@ -38,7 +38,7 @@ public class WebConfigurer implements ServletContextInitializer, EmbeddedServlet
     }
 
     @Bean
-    @ConditionalOnProperty(name = "jhipster.cors.allowed-origins")
+    @ConditionalOnProperty(name = "benchmark.cors.allowed-origins")
     public CorsFilter corsFilter() {
         log.debug("Registering CORS filter");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
