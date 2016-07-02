5f4ec065b26d4751a5e58abcc63a448d44182b76
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-02 21:04:51 +0200

    Corrects join statement to allow one to select datasets by category

--- src/main/java/com/codinginfinity/benchmark/managenent/repository/DatasetRepository.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/repository/DatasetRepository.java
@@ -1,7 +1,11 @@
 package com.codinginfinity.benchmark.managenent.repository;
 
 import com.codinginfinity.benchmark.managenent.domain.Dataset;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by reinhardt on 2016/06/25.
@@ -9,4 +13,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DatasetRepository extends RepoEntityRepository<Dataset> {
 
+    @Override
+    @Query("select d.id, d.name, d.description, d.categories from Dataset d " +
+            "inner join d.categories dc where dc.id = :categoryId")
+    List<Dataset> findByCategory(@Param("categoryId") Long categoryId);
 }
