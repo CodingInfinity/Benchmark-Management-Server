7b809d2b8bd02747e546028eab722c6348b66691
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-02 22:31:19 +0200

    Updates user REST endpoint with typed exception which can be thrown by lower layers, as to allow unit tests to pass in order to test fulfillment of implementation to service contract

--- src/main/java/com/codinginfinity/benchmark/managenent/web/rest/UserResource.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/web/rest/UserResource.java
@@ -25,10 +25,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
