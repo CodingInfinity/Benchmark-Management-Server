7c9d9b3fe333d99035f804b04b74b8a49efdb15b
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-02 21:03:56 +0200

    Corrects join statement to allow one to select algorithms by category

--- src/main/java/com/codinginfinity/benchmark/managenent/repository/AlgorithmRepository.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/repository/AlgorithmRepository.java
@@ -1,7 +1,6 @@
 package com.codinginfinity.benchmark.managenent.repository;
 
 import com.codinginfinity.benchmark.managenent.domain.Algorithm;
-import com.codinginfinity.benchmark.managenent.domain.Category;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,7 +14,7 @@ import java.util.List;
 public interface AlgorithmRepository extends RepoEntityRepository<Algorithm> {
 
     @Override
-    @Query("select id, name, user, description, categories from Algorithm a " +
-            "inner join algorithm_algorithmCategory ac where a.id = ac.algorithm_id and ac.category_id = :categoryId")
+    @Query("select a.id, a.name, a.description, a.categories from Algorithm a " +
+            "inner join a.categories ac where ac.id = :categoryId")
     List<Algorithm> findByCategory(@Param("categoryId") Long categoryId);
 }
