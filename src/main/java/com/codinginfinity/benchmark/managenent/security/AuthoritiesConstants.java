1abf6e90f386185c48c22c16978e9bc172645dc0
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-06 14:04:58 +0200

    Adds unit test for AuthoritiesConstants and updates AuthoritiesConstants to ensure it fullfills all requirements as set out by unit tests

--- src/main/java/com/codinginfinity/benchmark/managenent/security/AuthoritiesConstants.java
+++ src/main/java/com/codinginfinity/benchmark/managenent/security/AuthoritiesConstants.java
@@ -5,6 +5,9 @@ package com.codinginfinity.benchmark.managenent.security;
  */
 public final class AuthoritiesConstants {
 
+    private AuthoritiesConstants() {
+    }
+
     public static final String ADMIN = "ROLE_ADMIN";
 
     public static final String USER = "ROLE_USER";
