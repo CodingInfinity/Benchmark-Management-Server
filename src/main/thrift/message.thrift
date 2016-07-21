788a636b9476b52957510c230d5caf756431e34b
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-21 19:54:04 +0200

    Creates Apache Thrift message specifications file for jobs

--- src/main/thrift/message.thrift
+++ src/main/thrift/message.thrift
@@ -1,7 +1,21 @@
 namespace cpp com.codinginfinity.benchmark.management.thrift.messages
 namespace java com.codinginfinity.benchmark.management.thrift.messages
 
-struct Message {
-	1: i32 key
-	2: string value	
+enum MeasurementType {
+	CPU,
+	MEM,
+	TIME
+}
+
+enum LanguageType {
+	JAVA
+}
+
+struct JobSpecification {
+	1: i32 experimentId;
+	2: i32 jobId;
+	3: LanguageType languageType;
+	4: MeasurementType measurementType;
+	3: binary dataset;
+	4: binary algorithm;
 }
