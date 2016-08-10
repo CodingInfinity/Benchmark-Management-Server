6fe2927cc38178152bc2e4b93bfd8b64d76e11ee
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-08-10 18:50:15 +0200

    Updated the format of the thrift objects

--- src/main/thrift/message.thrift
+++ src/main/thrift/message.thrift
@@ -1,7 +1,35 @@
 namespace cpp com.codinginfinity.benchmark.management.thrift.messages
 namespace java com.codinginfinity.benchmark.management.thrift.messages
 
-struct Message {
-	1: i32 key
-	2: string value	
+enum MeasurementType {
+	CPU,
+	MEM,
+	TIME
+}
+
+enum LanguageType {
+	JAVA
+}
+
+struct JobSpecificationMessage {
+	1: i32 experimentId;
+	2: i32 jobId;
+	3: LanguageType languageType;
+	4: MeasurementType measurementType;
+	5: binary dataset;
+	6: binary algorithm;
+	7: i32 timeout;
+	8: i32 probeInterval;
+}
+
+struct ResultMessage {
+	1: i32 experimentId;
+	2: i32 jobId;
+	3: list<Measurement> measurements
+
+}
+
+struct Measurement {
+	1: i64 timestamp
+	2: i32 value
 }
