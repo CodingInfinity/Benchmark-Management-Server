caedff96cd9880dad5165063bf0834dd4b25decf
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-08-11 12:34:27 +0200

    Structure of the Experiment Manegement implementation

---
+++ src/main/java/com/codinginfinity/benchmark/management/service/experimentManagement/ExperimentManegementImpl.java
@@ -0,0 +1,21 @@
+package com.codinginfinity.benchmark.management.service.experimentManagement;
+
+import com.codinginfinity.benchmark.management.service.experimentManagement.request.CreateExperimentRequest;
+import com.codinginfinity.benchmark.management.service.experimentManagement.request.SaveJobResultsRequest;
+import com.codinginfinity.benchmark.management.service.experimentManagement.respones.CreateExperimentResponse;
+import com.codinginfinity.benchmark.management.service.experimentManagement.respones.SaveJobResultsRespones;
+
+/**
+ * Created by reinhardt on 2016/08/11.
+ */
+public class ExperimentManegementImpl implements ExperimentManagement {
+    @Override
+    public SaveJobResultsRespones saveJobResults(SaveJobResultsRequest request) {
+        return null;
+    }
+
+    @Override
+    public CreateExperimentResponse createExpriment(CreateExperimentRequest request) {
+        return null;
+    }
+}
