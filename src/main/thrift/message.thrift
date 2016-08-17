d6fb667d43cd1aaca2b835676d9e0af0c7f31e63
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-08-17 12:09:10 +0200

    Resolved merge conflict on .thrift file

--- src/main/thrift/message.thrift
+++ src/main/thrift/message.thrift
@@ -11,16 +11,11 @@ enum LanguageType {
 	JAVA
 }
 
-<<<<<<< HEAD
 struct JobSpecificationMessage {
-=======
-struct JobSpecification {
->>>>>>> feature/repositoryManagement
 	1: i32 experimentId;
 	2: i32 jobId;
 	3: LanguageType languageType;
 	4: MeasurementType measurementType;
-<<<<<<< HEAD
 	5: binary dataset;
 	6: binary algorithm;
 	7: i32 timeout;
@@ -30,16 +25,10 @@ struct JobSpecification {
 struct ResultMessage {
 	1: i32 experimentId;
 	2: i32 jobId;
-	3: list<Measurement> measurements
-
+	3: list<Measurement> measurements;
 }
 
 struct Measurement {
-	1: i32 timestamp
-	2: i32 value
+	1: i32 timestamp;
+	2: i32 value;
 }
-=======
-	3: binary dataset;
-	4: binary algorithm;
-}
->>>>>>> feature/repositoryManagement
